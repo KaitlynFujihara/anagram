a966013b3e09c7cb821379a8846fc7b695a050fa
Guest User <Guest@Epicodus-8C.local>
2018-02-09 09:55:55 -0800

    first test pass. split into array

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,19 @@
-class Anagram
-  new_word = Anagram.new()
 
+
+class Anagram
   def initialize(word)
     @word = word
   end
 
-  def anagram_check()
+  def new_array(new_word)
+    word_array= new_word.split(" ")
+
   end
+  # def anagram_check()
+  # end
+  puts 'enter two words'
+  word = gets.chomp
+  new_word = Anagram.new(word)
+  new_word.new_array(word)
 
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,7 +3,12 @@ require('anagram')
 require('pry')
 
 describe('#anagram') do
-  it('check to see if two words have the same letters and return "These words are anagrams" if true') do
-  expect(new_word = Anagram.new(['tea','eat']).anagram_check).to(eq('These words are anagrams'))
+  it('turn two given words into an array') do
+    new_word = Anagram.new("tea eat")
+    expect(new_word.new_array("tea eat")).to(eq(['tea','eat']))
   end
+
+  # it('check to see if two words have the same letters and return "These words are anagrams" if true') do
+  # expect(new_word = Anagram.new(['tea','eat']).anagram_check).to(eq('These words are anagrams'))
+  # end
 end
